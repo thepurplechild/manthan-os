f30aa1dbb67a8c18fb778b32e37d8a8828c69522
Ambar Walia <ambarwalia@gmail.com>
2025-08-17 17:06:58 +0530

    Update page.tsx

--- apps/web/app/studio/page.tsx
+++ apps/web/app/studio/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useState } from "react";
 import { useAuth } from "../../src/components/AuthProvider";
 import { api } from "../../src/lib/api";
